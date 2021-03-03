dba5eeddc5df8dd1689d282600df2e8b71e2e39c
franco555 <razon20m@gmail.com>
2021-03-03 18:02:25 -0300

    terminado forgot password
    en controller

--- lib/app/modules/auth/forgotpassword/forgotpassword_controller.dart
+++ lib/app/modules/auth/forgotpassword/forgotpassword_controller.dart
@@ -1,5 +1,51 @@
+import 'package:caysa2021/app/routes/app_routes.dart';
+import 'package:caysa2021/app/utils/colors.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ForgotPasswordController extends GetxController{
   
+  String _email="";
+
+  void onEmailChanged(String text){ _email=text;}
+
+  void submitForgotPassword(){
+    if(this._email.trim()==""){ 
+      snackBarPersonal("Se necesita un E-mail");
+      return;
+    }
+
+    bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(this._email);
+    
+    if(emailValid){
+      goResetPassword();
+    }else{
+      snackBarPersonal("E-mail inválido");
+      return;
+    }
+    print(this._email);
+  }
+
+  void goLogin(){
+    Get.toNamed(AppRoutes.LOGIN);
+  }
+
+  void goResetPassword(){
+    Get.toNamed(AppRoutes.LOGIN);
+  }
+
+  void snackBarPersonal(String msg){
+     Get.snackbar(
+        "Cuidado!", 
+        msg,
+        snackPosition: SnackPosition.BOTTOM,
+        icon: Icon(Icons.warning, color: CF.colorTextSnackBarSplash(),), 
+        shouldIconPulse: true,
+        isDismissible: true,
+        duration: Duration(seconds: 3),
+        colorText: CF.colorTextSnackBarSplash(),
+        backgroundColor: CF.colorDanger()
+      );
+   }
+
 }
--- lib/app/modules/auth/forgotpassword/widgets/body_forgotpassword.dart
+++ lib/app/modules/auth/forgotpassword/widgets/body_forgotpassword.dart
@@ -1,3 +1,4 @@
+import 'package:caysa2021/app/modules/auth/forgotpassword/forgotpassword_controller.dart';
 import 'package:caysa2021/app/modules/auth/forgotpassword/widgets/decoration/background_forgotpassword.dart';
 import 'package:caysa2021/app/modules/auth/login/local_widgets/dinamic/goLogin_login.dart';
 import 'package:caysa2021/app/modules/auth/widgets/rounded_button_auth.dart';
@@ -7,6 +8,7 @@ import 'package:caysa2021/app/utils/colors.dart';
 import 'package:caysa2021/app/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
 
 class BodyForgotPassword extends StatelessWidget {
   const BodyForgotPassword({
@@ -15,22 +17,24 @@ class BodyForgotPassword extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(8.0,20.0,8.0,20.0),
-      child: BackgroundForgotPassword(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SvgPicture.asset(Constants.LOGO_WITH_NAME_WHITE,height: 150.0,),
-              TitlesAuth(title:"Recuperar Contraseña"),
-              RoundedInputFieldAuth(hintText: "Email", onChanged: (value) {},),
-              RoundedButtonAuth(color: CF.colorInfo(),fw: FontWeight.w400,sizeText: 16,text: "Enviar", press: (){}, ),
-              GoLogin( press: (){}),
-            ],
+    return GetBuilder<ForgotPasswordController>(
+      builder: (_)=>Padding(
+        padding: const EdgeInsets.fromLTRB(8.0,20.0,8.0,20.0),
+        child: BackgroundForgotPassword(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SvgPicture.asset(Constants.LOGO_WITH_NAME_WHITE,height: 150.0,),
+                TitlesAuth(title:"Recuperar Contraseña"),
+                RoundedInputFieldAuth(hintText: "Email", onChanged: _.onEmailChanged,),
+                RoundedButtonAuth(color: CF.colorInfo(),fw: FontWeight.w400,sizeText: 16,text: "Enviar", press: _.submitForgotPassword, ),
+                GoLogin( press: _.goLogin),
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
